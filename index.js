1a80735c75667876d596740eb29a7ad7e77a0fd8
Harsh Patel <Patelharsh241999@gmail.com>
2024-02-05 16:21:07 +0530

    * Initialized app

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+var bodyParser = require("body-parser");
+const data = require("./data/data.json");
+const path = require("path");
+
+const app = express();
+const port = 5500;
+
+app.use(bodyParser.json());
+
+app.get("/api/restaurants/list/v5", (_, res) => {
+  res.json(data);
+});
+
+app.get("/hello", (_, res) => {
+  res.send("app is working on " + port);
+});
+app.listen(port, () => {
+  console.log(` app listening on port ${port}`);
+});
+
+app.get("/api/js/app.9e9cdc4db42e535.min.js", (_, res) => res.sendFile(path.join(__dirname, "/public/api/js/app.9e9cdc4db42e535.min.js")));
+app.get("*", (_, res) => res.sendFile(path.join(__dirname, "/index.html")));
