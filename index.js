137bed8d948c4bf9762093ccede95d4eabd7f71b
Harsh Patel <Patelharsh241999@gmail.com>
2024-02-06 05:52:05 +0530

    * Added Mobile screen.

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const bodyParser = require("body-parser");
 
 const ddata = require("./data/ddata.json");
 const mdata = require("./data/mdata.json");
-const useragent = require('express-useragent');
+const useragent = require("express-useragent");
+const path = require("path");
+const app = express();
+const port = 5500;
+app.use(bodyParser.json()), app.use(useragent.express());
 
-(path = require("path")), (app = express()), (port = 5500);
-app.use(bodyParser.json()),
-app.use(useragent.express());
-
-  app.get("/api/restaurants/list/v5", (e, p) => {
-    // console.log("e", e);
-    p.json(ddata);
-  }),
+app.get("/api/restaurants/list/v5", (e, p) => {
+  // console.log("e", e);
+  p.json(ddata);
+}),
   app.post("/api/restaurants/list/update", (e, p) => {
     // console.log("e", e);
     p.json(mdata);
@@ -28,14 +28,12 @@ app.get("/hello", (e, p) => {
   app.get("/api/js/app.9e9cdc4db42e535.min.js", (e, p) =>
     p.sendFile(path.join(__dirname, "/public/api/js/app.9e9cdc4db42e535.min.js"))
   ),
-
   app.get("/api/js/es5-app.e94d64c6e2c0b3e6.js", (e, p) =>
     p.sendFile(path.join(__dirname, "/public/api/js/es5-app.e94d64c6e2c0b3e6.js"))
   ),
-  
   app.get("/api/css/index.css", (e, p) => p.sendFile(path.join(__dirname, "/public/api/css/index.css"))),
   app.get("*", (e, p) => {
     const result = e.useragent;
     // console.log(result, "device");
-    p.sendFile(path.join(__dirname, result.isMobile?"/public/index-m.html":"/public/index.html"));
+    p.sendFile(path.join(__dirname, result.isMobile ? "/public/index-m.html" : "/public/index.html"));
   });
